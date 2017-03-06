b7e3004a8ec4ad830c48606fa08ea78c5e60a2a5
mnshellgren <mns.hellgren@gmail.com>
2017-03-06 15:46:13 +0100

    add draggable functionality to app. addd NoteContainer which is the container for whatever content is to be added

--- imports/ui/Wall.jsx
+++ imports/ui/Wall.jsx
@@ -1,16 +1,20 @@
 import React, { Component, PropTypes } from 'react';
- 
+
+
+import NoteContainer from './NoteContainer.jsx'; 
 // Wall component - represents a wall component in which notes are shown
 
 export default class Wall extends Component {
 
   renderNotecontainers() {
-    
+    return <NoteContainer/>
   }
 
   render() {
     return (
-      <div className="wall"></div>
+      <div className="wall">
+	{ this.renderNotecontainers()}	
+      </div>
     );
   }
 }
