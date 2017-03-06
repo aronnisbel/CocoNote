75c68d4f55ec66be17b036f84a09c636f604699f
Linnea <id15lbn@cs.umu.se>
2017-03-06 14:59:57 +0100

    Skapat en textruta

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
- 
+
 import Wall from './Wall.jsx';
- 
+
+
 // App component - represents the whole app
 // grunden i vår applikation
 
 export default class App extends Component {
- 
-  renderWallarea(){
-    return <Wall/>
-  }
 
   render() {
     return (
@@ -17,13 +14,13 @@ export default class App extends Component {
         <header>
           <h1>CocoNote!</h1>
         </header>
- 
+
         <main className="wall-area">
 
-	
-	 {this.renderWallarea()}
-	</main>   
-        
+	         <Wall/>
+
+	     </main>
+
       </div>
     );
   }
--- imports/ui/Wall.jsx
+++ imports/ui/Wall.jsx
@@ -1,16 +1,19 @@
 import React, { Component, PropTypes } from 'react';
- 
+
+import TextEdit from './TextEdit.jsx';
 // Wall component - represents a wall component in which notes are shown
 
 export default class Wall extends Component {
 
   renderNotecontainers() {
-    
+
   }
 
   render() {
     return (
-      <div className="wall"></div>
+      <div className="wall">
+        <TextEdit/>
+      </div>
     );
   }
 }
