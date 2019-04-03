c50ad2d950df2f331ac6d810f2d8ab542b2878eb
HeavyKladdkaka <aron.nisbel@hotmail.com>
2019-04-03 16:08:21 +0200

    Added facebook login to the website.

--- imports/ui/AccountsUIWrapper.jsx
+++ imports/ui/AccountsUIWrapper.jsx
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
- 
+
 export default class AccountsUIWrapper extends Component {
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons,
       ReactDOM.findDOMNode(this.refs.container));
+
+      Template.login.events({
+    'click .login-facebook': function(e) {
+        e.preventDefault();
+
+        Meteor.loginWithFacebook({requestPermissions: ['public_profile', 'email']}, function(err){
+            if (err) {
+                console.log('Handle errors here: ', err);
+            }
+        });
+    }
+  });
   }
   componentWillUnmount() {
     // Clean up Blaze view
--- server/main.js
+++ server/main.js
@@ -5,4 +5,24 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  ServiceConfiguration.configurations.remove({
+    service: "facebook"
+});
+
+ServiceConfiguration.configurations.insert({
+    service: "facebook",
+    appId: '162550288030364',
+    secret: 'bc2eb1c65f45e8aeb45a59482ea0cd8a'
+});
+
+Accounts.onCreateUser(function (options, user) {
+
+    if (!user.services.facebook) {
+        return user;
+    }
+    user.username = user.services.facebook.name;
+    user.emails = [{address: user.services.facebook.email}];
+
+    return user;
+});
 });
